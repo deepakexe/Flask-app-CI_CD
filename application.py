7c3dd31fea1254d4fb05c72eab634a6201846fc9
deepak <64362719+deepakexe@users.noreply.github.com>
2023-07-20 13:04:27 +0530

    Update application.py

--- application.py
+++ application.py
@@ -284,4 +284,4 @@ def stress(seconds):
     return redirect(url_for("info"))
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    application.run(host='0.0.0.0', port=80, debug=True)
