3aea19e2617a446081d4919946766b9c07dee004
deepak <64362719+deepakexe@users.noreply.github.com>
2023-07-21 14:28:26 +0530

    Update kill.sh

--- kill.sh
+++ kill.sh
@@ -1,4 +1,4 @@
 #Bash script to kill the process using the port passed as an argument while running the script  example : ./kill.sh 8000
 #!/bin/bash
-kill -9 `ss -tunlp | grep $1 |awk '{print $7}'|awk -F "," '{print $2}'| awk -F "=" '{print $2}' `
+kill -9 `ss -tunlp | grep $1 |awk 'NR==1 {print $7}'|awk -F "," '{print $2}'| awk -F "=" '{print $2}' `
 
